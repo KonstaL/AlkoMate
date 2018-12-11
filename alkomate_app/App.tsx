97a3f5b6de8f02a9a8be72fa056add3571f84f66
Konsta Lehtinen <lehtinen.konsta@gmail.com>
2018-12-11 15:28:44 +0200

    Add react-navigation

--- alkomate_app/App.tsx
+++ alkomate_app/App.tsx
@@ -1,51 +1,42 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * 
- * Generated with the TypeScript template
- * https://github.com/emin93/react-native-template-typescript
- * 
- * @format
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image} from 'react-native';
+import {createStackNavigator, createBottomTabNavigator, createAppContainer} from 'react-navigation';
+import HomeScreen from './src/HomeScreen';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+const HomeStack = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  }
 });
+
+const DrinkStack = createStackNavigator({
+  Home: {
+    screen: HomeScreen // FIXME: temporary, not the real screen
+  }
+});
+
+const ProfileStack = createStackNavigator({
+  Home: {
+    screen: HomeScreen // FIXME: temporary, not the real screen
+  }
+});
+
+const TabNavigator = createBottomTabNavigator({
+  Home: HomeStack,
+  Drinks: DrinkStack,
+  Profile: ProfileStack,
+});
+
+const AppContainer = createAppContainer(TabNavigator);
+
 
 interface Props {}
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <AppContainer />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
---
+++ alkomate_app/src/HomeScreen.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Text, SafeAreaView } from "react-native";
+
+interface Props {
+
+}
+
+export default class App extends Component<Props> {
+    render() {
+        return(
+            <SafeAreaView>
+                <Text>HomeScreen</Text>
+            </SafeAreaView>
+        )
+    }
+}
