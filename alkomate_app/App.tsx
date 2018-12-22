1bd73bf6e89f3c6a1c5ef286680f9f0207fdf5ba
Konsta Lehtinen <lehtinen.konsta@gmail.com>
2018-12-22 16:01:31 +0200

    Move routing config to a seperate file

--- alkomate_app/App.tsx
+++ alkomate_app/App.tsx
@@ -1,57 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image} from 'react-native';
-import {createStackNavigator, createBottomTabNavigator, createAppContainer, createSwitchNavigator} from 'react-navigation';
-import HomeScreen from './src/components/HomeScreen';
-import ListScreen from './src/components/ListScreen';
-import DetailScreen from './src/components/BeverageScreen';
-import SignInScreen from './src/components/SignInScreen';
-import AuthLoadingComponent from './src/components/AuthLoading';
-import ProfileComponent from './src/components/Profile';
-
-const HomeStack = createStackNavigator({
-  Home: {
-    screen: HomeScreen
-  }
-});
-
-const DrinkStack = createStackNavigator({
-  Home: {
-    screen: ListScreen 
-  },
-  Details: {
-    screen: DetailScreen,
-  }
-});
-
-const ProfileStack = createStackNavigator({
-  Home: {
-    screen: ProfileComponent 
-  }
-});
-
-const TabNavigator = createBottomTabNavigator({
-  Home: HomeStack,
-  Drinks: DrinkStack,
-  Profile: ProfileStack,
-});
-
-const AuthStack = createStackNavigator({
-   SignIn: SignInScreen 
-});
-
-const AppContainer =  createAppContainer(createSwitchNavigator(
-  {
-    AuthLoading: AuthLoadingComponent,
-    App: TabNavigator,
-    Auth: AuthStack,
-  },
-  {
-    initialRouteName: 'AuthLoading',
-  }
-));
-
-
-
+import AppContainer from './src/config/routes';
 
 interface Props {}
 export default class App extends Component<Props> {
